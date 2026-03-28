dda9500a6e3d315ae640f474122b0b1553ad882a
nguyenchicong <nguyenchicong.catdang@gmail.com>
2026-03-28 15:02:24 +0700

    up

---
+++ Wp/MainWpServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ncc\Wp;
+
+use Illuminate\Support\AggregateServiceProvider;
+
+class MainWpServiceProvider extends AggregateServiceProvider {
+    protected $providers = [
+        
+    ];
+}
---
+++ Wp/WpServiceProvider.php
@@ -0,0 +1,9 @@
+<?php
+namespace Ncc\Wp;
+
+use Illuminate\Support\ServiceProvider;
+
+class WpServiceProvider extends ServiceProvider
+{
+    
+}
