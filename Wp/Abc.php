33aba2c429f271ea0907b5ecbd64de2ce26e990d
nguyenchicong <nguyenchicong.catdang@gmail.com>
2026-03-28 14:21:30 +0700

    up test

---
+++ Wp/Abc.php
@@ -0,0 +1,12 @@
+<?php
+namespace Ncc\Wp;
+class Abc
+{
+    public static function abc()
+    {
+        return 'abc';
+    }
+}
+
+// \Ncc\Wp::abc()
+//     "$schema": "https://getcomposer.org/schema.json",
