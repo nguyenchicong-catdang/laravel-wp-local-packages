ad0aa1746aea9fd004710c34d8edc657d65f81f7
nguyenchicong <nguyenchicong.catdang@gmail.com>
2026-03-31 08:56:11 +0700

    up

--- Wp/WpServiceProvider.php
+++ Wp/WpServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 namespace Ncc\Wp;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class WpServiceProvider extends ServiceProvider
@@ -8,8 +9,11 @@ class WpServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->loadRoutesFrom(__DIR__ . '/routesWp.php');
-        // $this->loadViewsFrom(__DIR__ . '/views', 'wp-view');
-        // Blade::anonymousComponentPath(__DIR__ . '/views/components', 'wp-comp');
+        $this->loadViewsFrom(__DIR__ . '/views', 'wp-view');
+        Blade::anonymousComponentPath(__DIR__ . '/components', 'wp-comp');
+        // 1. Phải có dòng này để định nghĩa "nhà" cho prefix wp-comp
+        // Blade::componentNamespace('Ncc\\Wp\\Components', 'wp-comp');
         // Blade::componentNamespace('Vendorpath\\Wp\\Components', 'wp-compName');
+
     }
 }
--- Wp/routesWp.php
+++ Wp/routesWp.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function() {
-    return 'abc';
+    return view('wp-view::index');
 });
